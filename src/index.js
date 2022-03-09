35525a7892e517a9d160c8e3f5dc209175592fd8
David Valentine <david_valentine@rapid7.com>
2022-03-09 12:26:03 +0000

    Remove codepencency as a dependency, use builtin node features instead

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
-const codependency = require('codependency');
-
 const Logger = require('./logger');
 const provisionWinston = require('./winston');
 const buildBunyanStream = require('./bunyanStream');
 
+try {
+  //  All the code below optionally configures winston if it's installed
+  const winston = require('winston');
+  const Transport = require('winston-transport');
 
-//  All the code below optionally configures winston, we need to do this
-//  here since we can only use codependency within the `package.json` `"main"`
-//  script, which gives us the correct `module`. Otherwise it errors out
-
-//  Use codependency for dynamically loading winston
-const requirePeer = codependency.register(module);
-
-//  Import winston
-const winston = requirePeer('winston', {optional: true});
-const Transport = requirePeer('winston-transport', {optional: true});
-
-//  If we have successfully loaded winston (user has it)
-//  we initialize our InsightTransport
-if (winston && Transport) {
-  provisionWinston(winston, Transport);
+  //  If we have successfully loaded winston (user has it)
+  //  we initialize our InsightTransport
+  if (winston && Transport) {
+    provisionWinston(winston, Transport);
+  }
+} catch (ignored) {
+  // If winston isn't installed, proceed as usual
 }
 
 //  Logger is default export
